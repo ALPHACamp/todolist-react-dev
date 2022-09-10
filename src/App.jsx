b57594c03384798aaf843378634938a8c4c38362
emily40830 <emily40830@gmail.com>
2022-09-10 17:10:26 +0800

    feat: init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import { TodoPage, LoginPage, SignUpPage } from './pages';
+
+function App() {
+  return (
+    <div className="app">
+      <SignUpPage />
+    </div>
+  );
+}
+
+export default App;
